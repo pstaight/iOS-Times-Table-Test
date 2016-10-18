d296f4aa60247e8b9cd54828ba5b0a2f2c13db88
Patrick Staight <patrickstaight@Patricks-Mac-mini.local>
2016-10-17 23:04:09 -0700

    Fixed the print statement.

--- Times Table Test/ViewController.swift
+++ Times Table Test/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("Ready to push")
+        print("Ready to push")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
